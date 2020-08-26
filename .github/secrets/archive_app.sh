ddcf74062de0f2905935919595d806924971bcdf
李广斌 <123456789@qq.com>
2020-08-27 00:14:09 +0800

    uplaod

--- .github/secrets/archive_app.sh
+++ .github/secrets/archive_app.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 
-xcodebuild -workspace package.xcworkspace -scheme package -sdk iphoneos -configuration Release -archivePath $PWD/build/package.xcarchive clean archive | xcpretty
+xcodebuild -workspace package.xcworkspace -scheme package -sdk iphoneos -configuration Release -archivePath $PWD/build/package.xcarchive CODE_SIGN_IDENTITY="Apple Development: YUANZHEN HAN (27J7882KKQ)" PROVISIONING_PROFILE="036fcfcc-9ad6-4358-b260-caf1a854ffc8" clean archive | xcpretty
 
 # xcodebuild -workspace package.xcworkspace \
 # 		   -scheme package \
