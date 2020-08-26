d527bac92a08ecbd8a0dcecf17108b2206bd212f
李广斌 <123456789@qq.com>
2020-08-26 22:12:05 +0800

    upload

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -6,7 +6,7 @@ gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS"  --output .github/s
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
-cp .github/secrets/match_AppStore_comtiagosantocalculatorIOS.mobileprovision.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/match_AppStore_comtiagosantocalculatorIOS.mobileprovision.mobileprovision
+cp .github/secrets/Universal_Links.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Universal_Links.mobileprovision
 
 
 security create-keychain -p "" build.keychain
