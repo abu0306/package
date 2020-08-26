5d86041fc662b24bafe038e8b0621c6d90dbf80d
李广斌 <123456789@qq.com>
2020-08-27 00:20:55 +0800

    uplaod

--- .github/secrets/archive_app.sh
+++ .github/secrets/archive_app.sh
@@ -3,12 +3,11 @@
 set -eo pipefail
 
 
-xcodebuild -workspace package.xcworkspace -scheme package  -configuration Release -archivePath $PWD/build/package.xcarchive clean archive | xcpretty
+# xcodebuild -workspace package.xcworkspace -scheme package  -configuration Release -archivePath $PWD/build/package.xcarchive clean archive | xcpretty
 
-# xcodebuild -workspace package.xcworkspace \
-# 		   -scheme package \
-# 		   -archivePath $PWD/build/package.xcarchive \
-# 		   -configuration Release \
-# 		   clean archive | xcpretty
+xcodebuild -workspace package.xcworkspace \
+		   -scheme package \
+		   -archivePath $PWD/build/package.xcarchive \
+		   -configuration Release 
 
 
